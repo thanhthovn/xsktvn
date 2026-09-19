2355b21d878ffea6762c5a1e0c8723da8c0fd0ea
thanhthovn <115600551+thanhthovn@users.noreply.github.com>
2026-09-19 08:46:48 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -34,8 +34,8 @@ OUTPUT_DIR = os.path.join(
 # ============ KHOẢNG NGÀY CẦN CÀO ============
 # TỰ SỬA 2 DÒNG NÀY KHI MUỐN ĐỔI KHOẢNG NGÀY
 # Định dạng: dd-mm-yyyy
-START_DATE = "01-01-2021"
-END_DATE = "31-12-2021"
+START_DATE = "01-01-2020"
+END_DATE = "31-12-2020"
 
 
 
